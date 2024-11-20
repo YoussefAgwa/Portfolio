7722608bea997109bc3ac8c5bb62dc009442c438
Youssef Ahmed <93931792+YoussefAgwa@users.noreply.github.com>
2024-11-20 04:29:56 +0200

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,18 @@
 let menuIcon = document.querySelector(".menu");
 let links = document.querySelector("ul.links");
-
+let links = document.querySelectorAll("ul.links li a");
 menuIcon.addEventListener("click", () =>{
   links.classList.add("mobile");
+})
+links.forEach(link => {
+  link.addEventListener("click" , (e) => {
+    e.target.classList.add("active");
+    links.forEach(link => {
+      if(link.classList.contains("active") && link != e.target){
+        link.classList.remove("active");
+      }
+    })
+  })
 })
 document.body.addEventListener("click", (e) => {
   if(e.target != menuIcon ) {
